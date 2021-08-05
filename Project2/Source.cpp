392854d827d18533d7bd097d9ff7aa4a6decb78f
elitsuri <zelitesuri@gmail.com>
2021-08-05 12:07:18 +0300

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,12 @@
+// --------------------- Include -----------------------
+#include <iostream>
+#include <string>
+// ---------------------- Using ------------------------
+using namespace std;
+// ----------------------- Main ------------------------
+int main()
+{
+	
+	cin.get(); cin.get();
+	return (EXIT_SUCCESS);
+}
